a752a5f5b3e1172a218155c1f7ccd5db0422bb95
Seto Elkahfi <setoelkahfi@gmail.com>
2018-03-20 11:53:53 +0700

    Initialize Xcode project

---
+++ VIPER IOS Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPER IOS Demo
+//
+//  Created by Seto Elkahfi on 23/03/18.
+//  Copyright © 2018 Seto Elkahfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

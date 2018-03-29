cc97df0bb221aea4d71d7917b838b7f1daed0021
Seto Elkahfi <setoelkahfi@gmail.com>
2018-03-28 21:38:53 -0800

    Add Pods to source control

---
+++ Pods/Target Support Files/Pods-VIPER IOS Demo/Pods-VIPER IOS Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_IOS_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_IOS_Demo
+@end
---
+++ Pods/Target Support Files/Pods-VIPER IOS DemoTests/Pods-VIPER IOS DemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPER_IOS_DemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_VIPER_IOS_DemoTests
+@end
